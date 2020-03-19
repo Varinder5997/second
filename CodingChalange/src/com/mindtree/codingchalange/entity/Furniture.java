8ea0a22047aaaeba40af0340cc9381f4a7cc7436
Varinder5997 <varinderbhatti3737@gmail.com>
2020-03-19 14:45:21 +0530

    first commit

---
+++ CodingChalange/src/com/mindtree/codingchalange/entity/Furniture.java
@@ -0,0 +1,33 @@
+package com.mindtree.codingchalange.entity;
+
+public class Furniture {
+	private int furnitureId;
+	private String furnitureName;
+	private double furnitureCost;
+	private int furnitureQuentity;
+	public int getFurnitureId() {
+		return furnitureId;
+	}
+	public void setFurnitureId(int furnitureId) {
+		this.furnitureId = furnitureId;
+	}
+	public String getFurnitureName() {
+		return furnitureName;
+	}
+	public void setFurnitureName(String furnitureName) {
+		this.furnitureName = furnitureName;
+	}
+	public double getFurnitureCost() {
+		return furnitureCost;
+	}
+	public void setFurnitureCost(double furnitureCost) {
+		this.furnitureCost = furnitureCost;
+	}
+	public int getFurnitureQuentity() {
+		return furnitureQuentity;
+	}
+	public void setFurnitureQuentity(int furnitureQuentity) {
+		this.furnitureQuentity = furnitureQuentity;
+	}
+	
+}
